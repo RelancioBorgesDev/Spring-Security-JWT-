a0b284c765eeaf7424c950aa63ad781a044c5fc4
RelancioBorgesDev <relancio.dev@gmail.com>
2023-02-14 16:25:31 -0300

    First Commit

---
+++ src/main/java/com/relnc/security/model/User.java
@@ -0,0 +1,27 @@
+package com.relnc.security.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+}
---
+++ src/test/java/com/relnc/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relnc.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
