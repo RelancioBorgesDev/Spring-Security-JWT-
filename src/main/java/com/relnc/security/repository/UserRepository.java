932ad62ec7912803a55533ab7b1f4a3c4f92ce42
RelancioBorgesDev <relancio.dev@gmail.com>
2023-02-24 15:03:50 -0300

    Repository

--- src/main/java/com/relnc/security/repository/UserRepository.java
+++ src/main/java/com/relnc/security/repository/UserRepository.java
@@ -1,5 +1,16 @@
 package com.relnc.security.repository;
 
-public class UserRepository {
+import com.relnc.security.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+
+
+
+
 
 }
