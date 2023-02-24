b568722259be226a3312de1bd261c702aab98b95
RelancioBorgesDev <relancio.dev@gmail.com>
2023-02-24 15:47:05 -0300

    Config & Service

---
+++ src/main/java/com/relnc/security/service/JwtService.java
@@ -0,0 +1,12 @@
+package com.relnc.security.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    //
+    public String extractUsername(String jwt){
+        return null;
+    }
+
+}
